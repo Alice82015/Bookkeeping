04b4e735d25c50efe4c4e15f5022a4505b48c7db
m305-8\w401 <a106082015@mail.shu.edu.tw>
2018-05-17 18:07:13 +0800

    first

---
+++ todolist/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todolist
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // 產生一個方塊
+            TodoItem item = new TodoItem();
+
+            // 放到 Todostack
+            TodoStack.Children.Add(item);
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 產生一個儲存每個 list 文字的 list
+            List<string> all = new List<string>();
+
+            // 取得每一個 item 的文字
+            foreach ( TodoItem item in TodoStack.Children )
+            {
+                all.Add(item.GetTaskName());
+            }
+
+            // 寫入檔案
+            System.IO.File.WriteAllLines(@"c:\tododate.txt",all);
+
+        }
+    }
+}
